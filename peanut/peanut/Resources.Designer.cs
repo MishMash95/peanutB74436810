d37cc9c5afc28f0e00ff6f35ea640d6aa917e8d3
Roy Miles <roy.miles12@gmail.com>
2015-12-21 02:55:49 +0000

    Fixed shit p.s sqlite is still being a bitch

--- peanut/peanut/Resources.Designer.cs
+++ peanut/peanut/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace peanut {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("poker_bot.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("peanut.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
